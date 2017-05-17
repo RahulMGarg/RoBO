4deaac7d8e4bc263639ffa828e9d4b7e1817cb9b
Jason Hartford <jasonhar@cs.ubc.ca>
2017-05-16 18:41:55 -0700

    simplified setup

--- setup.py
+++ setup.py
@@ -4,16 +4,9 @@ from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 
 requires = [
-    'DIRECT',
-    'george',
-    'emcee',
-    'cma',
     'scipy >= 0.12',
     'numpy >= 1.7'
     ]
-dependency_links = [
-      'https://github.com/sfalkner/george/tarball/master#egg=george'
-      ]
 
 optional_dependencies = [
     'cython',
@@ -25,19 +18,6 @@ optional_dependencies = [
     'pymatbridge'
     ]
 
-opt_dependency_links = {
-    'rf': 'https://bitbucket.org/aadfreiburg/random_forest_run/get/master.zip#egg=rf'
-}
-
-user_provided_depend = sys.argv[2:len(sys.argv)]
-for i in user_provided_depend:
-    dependency_name = i[2:len(i)]
-    if dependency_name in optional_dependencies:
-        if dependency_name in opt_dependency_links:
-            dependency_links.append(opt_dependency_links[dependency_name])
-        requires.append(dependency_name)
-        sys.argv.remove(i)
-
 setup(name='RoBO',
       version='0.2.1',
       description='Framework for Bayesian optimization',
@@ -48,5 +28,4 @@ setup(name='RoBO',
       packages=find_packages(),
       license='LICENSE.txt',
       test_suite='robo',
-      install_requires=requires,
-      dependency_links=dependency_links)
+      install_requires=requires)
